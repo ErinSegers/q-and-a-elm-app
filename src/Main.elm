d99c3ef22e58bda17bc098081e98079d5ff7b67e
Erin McCaffrey <>
2019-09-04 16:45:03 -0400

    creates reset option and first name input

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,77 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Attribute, Html, button, div, input, text)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    { count : Int
+    , firstName : String
+    , lastName : String
+    }
+
+
+init : Model
+init =
+    Model 0 "firstname" "lastname"
 
 
 type Msg
     = Increment
     | Decrement
+    | Reset
+    | FirstName String
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            let
+                newModel =
+                    { model | count = model.count + 1 }
+            in
+            newModel
 
         Decrement ->
-            model - 1
+            let
+                newModel =
+                    { model | count = model.count - 1 }
+            in
+            newModel
+
+        Reset ->
+            let
+                newModel =
+                    { model | count = 0 }
+            in
+            newModel
+
+        FirstName input ->
+            let
+                newModel =
+                    { model | firstName = input }
+            in
+            newModel
 
 
+view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ div []
+            [ button [ onClick Decrement ] [ text "-" ]
+            , div [] [ text (String.fromInt model.count) ]
+            , button [ onClick Increment ] [ text "+" ]
+            ]
+        , div []
+            [ button [ onClick Reset ] [ text "reset" ]
+            ]
+        , input [ value model.firstName, onInput FirstName ] []
+        , div [] [ text model.firstName ]
         ]
